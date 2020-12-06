69794a89473ecf18d046092e4da40740fb7c8ac1
arhayem <72758944+arhayem@users.noreply.github.com>
2020-12-06 12:56:35 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 console.log('...building...');
 console.log('Client ID: ' + process.env.OKTA_OAUTH2_CLIENT_ID_SPA);
 console.log('Okta Org Url: ' + process.env.OKTA_CLIENT_ORGURL);
-process.env.VUE_APP_OKTA_OAUTH2_CLIENT_ID_SPA='0oa1s9mm0pajBwi2e5d6';
-process.env.VUE_APP_OKTA_CLIENT_ORGURL='https://dev-4491726.okta.com';
+process.env.VUE_APP_OKTA_OAUTH2_CLIENT_ID_SPA= 0oa1s9mm0pajBwi2e5d6;
+process.env.VUE_APP_OKTA_CLIENT_ORGURL= https://dev-4491726.okta.com;
